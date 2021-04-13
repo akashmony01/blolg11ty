642def7d380a9c3fb81b0730174e8e7ebe4ec7a4
akashmony01 <akashmonya88@gmail.com>
2021-04-13 21:55:29 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,20 @@ module.exports = {
       padding: "2rem",
     },
     extend: {
-      fontFamily: {
-        grandRoyal: ['Grand Royal'],
-        goldplay: ['Goldplay'],
-      },
       colors: {
-        'bgColor': '#FFF9E4',
-        'primary': '#121212',
-        'secondery': '#368F8B',
-      }
+        primary: '#152251',
+        darkPrimary: '#9B0F0D',
+        secondery: '#D72B27',
+        textColor: '#161A1D',
+        textLight: '#8E94A8',
+      },
+      fontFamily: {
+        gtWalsheimLight: ['gt-walsheim-light'],
+        gtWalsheimRegular: ['gt-walsheim-regular'],
+        gtWalsheimMedium: ['gt-walsheim-medium'],
+        gtWalsheimBold: ['gt-walsheim-bold'],
+      },
+      filter: ['hover', 'focus'],
     },
   },
   variants: {
